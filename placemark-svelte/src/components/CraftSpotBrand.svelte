5bf59c28e3e9d9bf3daea39ccc509b96c23987fc
Fiona Waters <fiwaters6@gmail.com>
2022-05-21 22:13:19 +0100

    main page created with svelte

---
+++ placemark-svelte/src/components/CraftSpotBrand.svelte
@@ -0,0 +1,4 @@
+<div class="title p-3 is-flex">
+    <i style="font-size: 48px; color:rgb(241, 211, 41)" class="fas fa-mitten"></i>
+    <div class="ml-4">CraftSpot </div>
+  </div>
---
+++ placemark-svelte/src/pages/Main.svelte
@@ -0,0 +1,22 @@
+<script>
+    
+import craftSupplies from "/src/assets/craftSupplies.jpg";
+import WelcomeNavigator from "../components/WelcomeNavigator.svelte";
+
+</script>
+
+
+<div class="columns is-vcentered">
+    <div class="column is-one-third-right">
+        <WelcomeNavigator/>
+    </div>
+    
+</div>
+<div class="columns is-vcentered ">
+    <figure class="image">
+          <img width="300" class="is-rounded" src="{craftSupplies}" alt="craft supplies">
+    </figure>
+    <div class="column is-right">
+    <h1 class="title has-text-right">Please sign up or log in...</h1>
+  </div>
+    </div>
